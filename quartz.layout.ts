ecd1ef3c3245b107a2c5413049fbe998378e2784
Monon00ke <monon00ke@gmail.com>
2026-06-01 18:17:09 +0300

    config: make graph global like Obsidian

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({ localGraph: false }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
